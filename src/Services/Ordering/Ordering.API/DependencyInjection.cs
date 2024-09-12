1da3d54b9d7855ce45640d280af8a9ef76ee4e5f
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-09-12 16:03:29 +0700

    Create Codebase Clean Architecture DDD

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            app.MapCarter();
+
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+            return app;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,14 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Ordering API!");
